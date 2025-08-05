3563f4dfa1740d4d6ca275dfd68f7a5ad555f484
Edgar Ampiire <eampiire@gmail.com.com>
2025-08-05 21:41:22 +0300

    Added player_spec test

---
+++ spec/player_spec.rb
@@ -0,0 +1,9 @@
+require_relative './lib/tic_tac_toe/player'
+
+Rspec.describe Player do
+    it "initializes with a name and marker" do
+      player = Player.new('Alice', 'X')
+      expect(player.name).to eq('Alice')
+      expect(player.marker).to eq('X')
+    end
+  end
